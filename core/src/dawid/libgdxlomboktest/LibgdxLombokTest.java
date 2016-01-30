de7db193a5fe2ce786fcfb67108eca25ff1ac82d
BuildTools <dawidrusin90@gmail.com>
2016-01-30 22:50:55 +0100

    Using lombok in core and adding lombok to the gradle.build files

--- core/src/dawid/libgdxlomboktest/LibgdxLombokTest.java
+++ core/src/dawid/libgdxlomboktest/LibgdxLombokTest.java
@@ -1,14 +1,24 @@
 package dawid.libgdxlomboktest;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+
+import lombok.Getter;
+import lombok.Setter;
 
 public class LibgdxLombokTest extends ApplicationAdapter {
 
-	@Override
-	public void create () {
-	}
+	Test test = new Test();
 
 	@Override
 	public void render () {
+		Gdx.app.log("delta", Float.toString(test.getDelta()));
+		test.setDelta(Gdx.graphics.getDeltaTime());
+	}
+
+	@Getter
+	@Setter
+	static class Test {
+		private float delta = 0;
 	}
 }
